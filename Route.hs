6a8ffeee0bd3e135f0b53aa5f23f814cb25cac05
Alex <yodabodner@gmail.com>
2025-03-09 16:30:33 +0000

    basico de route y Palet anda

---
+++ Route.hs
@@ -0,0 +1,22 @@
+module Route ( Route, newR, inOrderR )
+  where
+import Data.List (elemIndex)
+data Route = Rou [ String ] deriving (Eq, Show)
+
+newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
+
+newR routeList = Rou routeList
+
+inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
+inOrderR (Rou cities) city1 city2 | ind1 /= Nothing && ind2 /= Nothing = ind1 < ind2
+                           | otherwise = False 
+                    where  
+                        ind1 = elemIndex city1 cities 
+                        ind2 = elemIndex city2 cities
+
+-- Tests: 
+-- ghci> r = Rou ["schiaffi", "amado"]
+-- ghci> inOrderR r "amado" "schiaffi"
+-- False
+-- ghci> inOrderR r  "schiaffi" "amado"
+-- True
