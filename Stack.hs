b817762bac64d67ee84a7a9feead561b29493460
MoraVigo <moravigomalusardi2@gmail.com>
2025-03-15 16:42:21 -0300

    Fixed unload in Truck.hs and popS in Stack.hs

--- Stack.hs
+++ Stack.hs
@@ -7,7 +7,7 @@ import Route
 data Stack = Sta [ Palet ] Int deriving (Eq, Show) -- Stack es un tipo de dato. Sta es el constructor que recive una
 -- lista de Palets y un Int que debe soportar Eq y Show.
 
-newS :: Int -> Stack                      -- construye una Pila con la capacidad indicada 
+newS :: Int -> Stack                      -- construye una Pila con la capacidad de palets indicada 
 newS cap = Sta [] cap
                   
 freeCellsS :: Stack -> Int                -- responde la celdas disponibles en la pila
@@ -30,9 +30,12 @@ holdsS (Sta palets cap) palet ruta =
 popS :: Stack -> String -> Stack          -- quita del tope los paletes con destino en la ciudad indicada
 -- Entrada => Stack: La pila de palets actual, String: La ciudad donde se están descargando palets.
 -- Salida => Stack: La pila de palets actualizada.
+popS (Sta [] cap) _ = Sta [] cap  -- Caso base: si la pila está vacía, no hacemos nada
 popS (Sta palets cap) ciudad 
                     | destinationP (head palets) == ciudad = Sta (dropWhile (\p -> destinationP p == ciudad) palets) cap
-                    | otherwise = error "No se pueden desapilar Palets en esta ciudad"
+                    | otherwise = Sta palets cap -- Si el palet no tiene la ciudad destino igual a la ciudad donde se está descargando, no se desapila, devuelve el stack tal cual le llego.
+                    -- | otherwise = error "No se pueden desapilar Palets en esta ciudad"    ==> Lo comento porque sino en Truck tira error. El Truck
+                    -- chequea stack por stack. Si un stack no tiene nada para descargar en esa ciudad no quiero que tire error y se corte el programa.
 
 -- Se usa pattern matching para desestructurar el Stack. palets: Lista de palets apilados en la bahía. cap: Capacidad máxima de la bahía.
 -- No chequeamos que no se desapile uno que esta abajo de otro que no debe desapilarse piorque hacemos el chequeo
