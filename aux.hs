b9a47614ee60442f672d7c3296d1485f39b1e244
AlexBodner <61150961+AlexBodner@users.noreply.github.com>
2025-03-15 17:20:47 -0300

    Delete aux.hs

--- aux.hs
+++
@@ -1,6 +0,0 @@
-loadIntoFirstAvailable :: [Stack] -> Palet -> [Stack]
---Funcion recursiva que descompone la lista en 2: el primer elemento y el resto de la lista. Y se va llamando recursivamente hasta descomponerla toda o encontrar un hueco libre. 
-loadIntoFirstAvailable [] _ = []  -- 
-loadIntoFirstAvailable (primero:resto) palet
-  | freeCellsS primero > 0 && holdsS primero palet  = stackS primero palet : resto
-  | otherwise       = (primero : loadIntoFirstAvailable resto )palet
