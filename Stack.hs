caec00b571bca17efca211e96f91ef9c524dc1c2
MoraVigo <moravigomalusardi2@gmail.com>
2025-03-15 10:50:58 -0300

    modified popS in Stack

--- Stack.hs
+++ Stack.hs
@@ -4,7 +4,8 @@ module Stack ( Stack, newS, freeCellsS, stackS, netS, holdsS, popS )
 import Palet
 import Route
 
-data Stack = Sta [ Palet ] Int deriving (Eq, Show)
+data Stack = Sta [ Palet ] Int deriving (Eq, Show) -- Stack es un tipo de dato. Sta es el constructor que recive una
+-- lista de Palets y un Int que debe soportar Eq y Show.
 
 newS :: Int -> Stack                      -- construye una Pila con la capacidad indicada 
 newS cap = Sta [] cap
@@ -17,19 +18,22 @@ stackS (Sta palets cap) palet | length palets < cap && netS (Sta palets cap) + n
                               | otherwise = error "No hay lugar en la pila"
 
 netS :: Stack -> Int                      -- responde el peso neto de los paletes en la pila
-netS (Sta palets _) = sum (map netP palets)
+netS (Sta palets _) = sum (map netP palets) -- map aplica netP a cada uno de los elementos de la lista 'palets'
 
 holdsS :: Stack -> Palet -> Route -> Bool -- indica si la pila puede aceptar el palet considerando las ciudades en la ruta
 holdsS (Sta [] _) _ _ = True  -- Si la pila está vacía, acepta cualquier palet
 holdsS (Sta palets cap) palet ruta =
   length palets < cap  -- Verifica que haya espacio
   && netS (Sta palets cap) + netP palet <= 10  -- Verifica que no supere las 10 toneladas
-  && inOrderR ruta (destinationP (head palets)) (destinationP palet)  -- Verifica el orden de la ruta
+  && inOrderR ruta (destinationP palet) (destinationP (head palets))  -- Quiero que el palet que voy a agregar se baje antes que el head. 
 
 popS :: Stack -> String -> Stack          -- quita del tope los paletes con destino en la ciudad indicada
 -- Entrada => Stack: La pila de palets actual, String: La ciudad donde se están descargando palets.
 -- Salida => Stack: La pila de palets actualizada.
-popS (Sta palets cap) ciudad = Sta (dropWhile (\p -> destinationP p == ciudad) palets) cap
+popS (Sta palets cap) ciudad 
+                    | destinationP (head palets) == ciudad = Sta (dropWhile (\p -> destinationP p == ciudad) palets) cap
+                    | otherwise = error "No se pueden desapilar Palets en esta ciudad"
+
 -- Se usa pattern matching para desestructurar el Stack. palets: Lista de palets apilados en la bahía. cap: Capacidad máxima de la bahía.
 -- No chequeamos que no se desapile uno que esta abajo de otro que no debe desapilarse piorque hacemos el chequeo
 -- de nunca agregar un pallet a la pila cuya ciudad destino viene despues del de un pallet inferior. Al hacer ese chequeo
