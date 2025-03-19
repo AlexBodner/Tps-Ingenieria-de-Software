e448f9d25f01b0bb8220cae67b2a61af816b3adc
Alex Bodner <yodabodner@gmail.com>
2025-03-19 19:10:35 -0300

    fix en sintaxis de ruta y arreglados test

--- tp1/Route.hs
+++ tp1/Route.hs
@@ -9,8 +9,8 @@ newR routeList = Rou routeList
 
 inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
 inOrderR (Rou cities) city1 city2 | ind1 /= Nothing && ind2 /= Nothing = ind1 <= ind2
-                           | ind1 == Nothing "Error al ingresar las ciudades, la ciudad 1 no se encuentra en la ruta."
-                           | otherwise error "Error al ingresar las ciudades, la ciudad 2 no se encuentra en la ruta." 
+                           | ind1 == Nothing = error "Error al ingresar las ciudades, la ciudad 1 no se encuentra en la ruta."
+                           | otherwise = error "Error al ingresar las ciudades, la ciudad 2 no se encuentra en la ruta." 
                     where  
                         ind1 = elemIndex city1 cities 
                         ind2 = elemIndex city2 cities
---
+++ tp1/TestsUnitariosRoute.hs
@@ -0,0 +1,24 @@
+import Route
+
+-- Crear función de testeo
+assert :: Bool -> String -> IO ()
+assert cond msg = putStrLn $ if cond then "✅ OK: " ++ msg else "❌ Falló: " ++ msg
+
+main :: IO ()
+main = do
+  -- Test: 2 ciudades
+  let r1 = newR ["Baires", "Cordoba"] 
+  assert(not (inOrderR r1 "Cordoba" "Baires")) "Encuentra que la condicion es Falsa con 2 ciudades"
+  assert( inOrderR r1  "Baires" "Cordoba") "Encuentra que la condicion es Verdadera con 2 ciudades"
+
+  -- Test 3 ciudades
+  let r2 = newR ["Baires", "Rosario","Cordoba"] 
+  assert(not (inOrderR r2 "Cordoba" "Baires")) "Encuentra que la condicion es Falsa con 3 ciudades. Compara 2 contra 0."
+  assert(not (inOrderR r2 "Cordoba" "Rosario")) "Encuentra que la condicion es Falsa con 3 ciudades. Compara 2 contra 1."
+
+  assert(not (inOrderR r2 "Rosario" "Baires")) "Encuentra que la condicion es Falsa con 3 ciudades. Compara 1 contra 0."
+
+  assert( inOrderR r2  "Baires" "Cordoba") "Encuentra que la condicion es Verdadera con 3 ciudades. Compara 0 contra 2"
+  assert( inOrderR r2  "Baires" "Rosario") "Encuentra que la condicion es Verdadera con 3 ciudades. Compara 0 contra 1"
+  assert( inOrderR r2  "Rosario" "Cordoba") "Encuentra que la condicion es Verdadera con 3 ciudades. Compara 1 contra 2"
+  
