c755786c64aeb19febfa3177a240d2fc2943ed47
moravigo <vigomalusardim@udesa.edu.ar>
2025-05-20 15:43:57 -0300

    implemente reverse

--- TP3/src/Uno/JuegoUNO.java
+++ TP3/src/Uno/JuegoUNO.java
@@ -1,12 +1,12 @@
 package Uno;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class JuegoUNO {
     ArrayList<Carta> mazo;
     ArrayList<Jugador> jugadores;
     Carta cartaPozo = new CartaVacia();
-
 
     public JuegoUNO(ArrayList<Jugador> jugadores, ArrayList<Carta> mazo, ArrayList<Integer> cantidadPorJugador) {
         this.jugadores = jugadores;
@@ -15,26 +15,37 @@ public class JuegoUNO {
         int index_cant = 0;
         for (Jugador jugador : jugadores) {
             int cantidad = cantidadPorJugador.get(index_cant);
-            jugador.recibirCartas(mazo.subList(i, i+cantidad));
+            jugador.recibirCartas(mazo.subList(i, i + cantidad));
             i += cantidad;
             index_cant += 1;
         }
     }
 
-    public JuegoUNO() {}
+    public JuegoUNO() {
+    }
 
-    public Carta getPozo(){
+    public Carta getPozo() {
         return cartaPozo;
     }
 
-    public Jugador getJugador(String nombre){
+    public Jugador getJugador(String nombre) {
         return jugadores.stream().filter(j -> j.getNombre().equals(nombre)).findFirst().orElse(null);
     }
 
-    public Jugador getJugadorTurno(){
+    public Jugador getJugadorTurno() {
         // Tomamos el primero y lo ponemos ultimo en la lista
         Jugador jugador = jugadores.removeFirst();
         jugadores.add(jugador);
         return jugador;
     }
+
+    public JuegoUNO reverse(){
+        Collections.reverse(jugadores);
+        Jugador jugador = jugadores.removeFirst();
+        jugadores.add(jugador);
+        return this;
+    }
+
 }
+
+
--- TP3/src/Uno/TestsUno.java
+++ TP3/src/Uno/TestsUno.java
@@ -20,9 +20,19 @@ public class TestsUno {
     }
 
     @Test
-    public void test03DosJugadores4CartasTurno1(){
+    public void test03DosJugadores4CartasTurnos(){
         JuegoUNO juego = juego2Jugadores4Cartas();
         assertEquals( "J1", juego.getJugadorTurno().getNombre() );
+        assertEquals( "J2", juego.getJugadorTurno().getNombre() );
+        assertEquals( "J1", juego.getJugadorTurno().getNombre() );
+    }
+
+    @Test
+    public void test04CuatroJugadores8CartasReverse(){
+        JuegoUNO juego = juego4Jugadores8Cartas();
+        assertEquals( "J1", juego.getJugadorTurno().getNombre() );
+        assertEquals( "J2", juego.getJugadorTurno().getNombre() );
+        assertEquals( "J1", juego.reverse().getJugadorTurno().getNombre() );
     }
 
     private static JuegoUNO juego2Jugadores4Cartas() {
@@ -39,6 +49,37 @@ public class TestsUno {
         mazo.add(new CartaNumerica("verde", 1));
 
         ArrayList<Integer> cantidadPorJugador = new ArrayList<>();
+        cantidadPorJugador.add(2);
+        cantidadPorJugador.add(2);
+
+        return new JuegoUNO(jugadores, mazo, cantidadPorJugador);
+    }
+
+    private static JuegoUNO juego4Jugadores8Cartas() {
+        Jugador j1 = new Jugador("J1");
+        Jugador j2 = new Jugador("J2");
+        Jugador j3 = new Jugador("J3");
+        Jugador j4 = new Jugador("J4");
+
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(j1);
+        jugadores.add(j2);
+        jugadores.add(j3);
+        jugadores.add(j4);
+
+        ArrayList<Carta> mazo = new ArrayList<>();
+        mazo.add(new CartaNumerica("rojo", 1));
+        mazo.add(new CartaNumerica("azul", 3));
+        mazo.add(new CartaNumerica("azul", 1));
+        mazo.add(new CartaNumerica("verde", 1));
+        mazo.add(new CartaNumerica("rojo", 4));
+        mazo.add(new CartaNumerica("verde", 3));
+        mazo.add(new CartaNumerica("amarillo", 3));
+        mazo.add(new CartaNumerica("amarillo", 1));
+
+        ArrayList<Integer> cantidadPorJugador = new ArrayList<>();
+        cantidadPorJugador.add(2);
+        cantidadPorJugador.add(2);
         cantidadPorJugador.add(2);
         cantidadPorJugador.add(2);
 
